cc3124716ce57228d4e139c7bfcc1704f49c5c36
Luis Sukys <luis.sukys@sippulse.com>
2020-08-24 15:48:33 -0300

    definição de versões para o swagger

--- src/main/java/br/com/lsukys/centraldeerros/config/SwaggerConfig.java
+++ src/main/java/br/com/lsukys/centraldeerros/config/SwaggerConfig.java
@@ -8,14 +8,11 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
 
-import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
@@ -35,17 +32,32 @@ public class SwaggerConfig {
 	@Bean
 	public Docket apis() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Application V1")
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.lsukys.centraldeerros"))
-				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("br.com.lsukys.centraldeerros.controller"))
+				.paths(PathSelectors.ant("/v1/**"))
 				.build()
 				.apiInfo(apiInfo())
 				.useDefaultResponseMessages(false);
 				//.securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
 		        //.securityContexts(Arrays.asList(securityContext()));
-		
 	}
 
+	@Bean
+	public Docket apisv2() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Application V2")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.lsukys.centraldeerros.controller"))
+				.paths(PathSelectors.ant("/v2/**"))
+				.build()
+				.apiInfo(apiInfo())
+				.useDefaultResponseMessages(false);
+				//.securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+		        //.securityContexts(Arrays.asList(securityContext()));
+	}
+	
+	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title(buildProperties.getName())
 				.description("Projeto com uma API Rest para centralizar registros de erros de aplicações.")
